01930948f978b545189f956bd5dfd7222ff41261
jimmy-lew <jimmylew9779@gmail.com>
2026-01-09 17:24:41 +0800

    feat: add opts for cors and route

--- psse.ts
+++ psse.ts
@@ -1,18 +1,38 @@
 import build from 'pino-abstract-transport'
 import { v4 as uuid } from 'uuid'
 import { App } from 'uWebSockets.js'
-import type { HttpResponse } from 'uWebSockets.js'
+import type { HttpResponse, HttpRequest } from 'uWebSockets.js'
 import {cors} from 'uws-cors'
+
+type Origin = string | RegExp | ((req: HttpRequest) => boolean | void);
+type HTTPMethod = 'GET' | 'HEAD' | 'POST' | 'PUT' | 'DELETE'
+type MaybeArray<T> = T | T[];
+interface CORSConfig {
+    origin?: Origin | boolean | Origin[];
+    methods?: boolean | undefined | null | '' | '*' | MaybeArray<HTTPMethod | (string & {})>;
+    allowedHeaders?: true | string | string[];
+    exposeHeaders?: true | string | string[];
+    credentials?: boolean;
+    maxAge?: number;
+    preflight?: boolean;
+}
 
 interface SSETransportOptions {
   port: number
+  route?: string
+  cors?: CORSConfig
 }
 
-export default async function (opts: SSETransportOptions = {port: 3333}) {
-  const app = cors(App())
+export default async function (opts: SSETransportOptions = {
+  port: 3333,
+  cors: {}
+}) {
+  const app = cors(App(), opts.cors)
   const connections: Record<string, HttpResponse> = {}
 
-  app.get("/", (conn) => {
+  const route = opts.route ?? '/'
+
+  app.get(route, (conn) => {
     const conn_id = uuid()
     conn.cork(() => {
       conn.writeHeader('Content-Type', 'text/event-stream')
@@ -28,8 +48,7 @@ export default async function (opts: SSETransportOptions = {port: 3333}) {
     })
 
   })
-  app.listen(opts.port, () => {})
-
+  .listen(opts.port, () => {})
 
   return build(async (stream) => {
     for await (const obj of stream) {
